ffff4076cedb60371bbc1a581e680fe6abd96f1c
ramzy <20121598@student.hhs.nl>
2023-03-09 13:45:53 +0100

    test 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("kess omak");
     }
     //this a test
     // this is a second test
