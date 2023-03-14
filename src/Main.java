a550848dcc9fc901ccb9895d014dd3a953a8c6c6
dhr.benali <dhr.benali@gmail.com>
2023-03-14 14:13:46 +0100

    first upload of a structure

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
+
+
+/* this is a program that produces quotations complying to the conditions of the client "bedrijf 42"
+it should run in a while loop, and we intend to work with the basis of a template */
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("kess omak");
+        boolean run = true;
+        Scanner scanner = new Scanner(System.in);
+        while(run) {
+            String input = scanner.nextLine(); //first version of inputting into console
+
+            //switch case for a bar-bones version of commands and results, classes and methods have yet to be added.
+            switch(input){
+                case "exit":
+                    run = false;
+                    break;
+                case "print":
+                    System.out.println("printing current quota");
+                    break;
+                case "finalize":
+                    System.out.println("printing and storing quota before shutting down");
+                    run = false;
+                    break;
+                case "list":
+                    System.out.println("listing possible options with prices and discounts");
+                    break;
+                case "add":
+                    System.out.println("function for adding options");
+                    break;
+                case "remove":
+                    System.out.println("function for removing options");
+                    break;
+                case "option 1":
+                    System.out.println("selecting or de-selecting option 1");
+                    break;
+                case "option 2":
+                    System.out.println("selecting or de-selecting option 2");
+                    break;
+
+            }
+        }
     }
-    //this a test
-    // this is a second test
-    //this is a third test
-    //this is a 4th test
 }
