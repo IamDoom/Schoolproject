d2de4acb0075eb870e24661aabf77b8029305228
dhr.benali <dhr.benali@gmail.com>
2023-03-20 11:36:53 +0100

    update first function for creation of a quote within which we will have a list of terms to create the boat inside the quote besically im imagining recursively using methods to create the finalized quote.

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,105 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class quote{
-    String clientName;
-    double price;
-    String date;
-    String orderNumber;
-    boat boat;
+class shell{
+    Scanner scanner = new Scanner(System.in);
+    public quote createQuote(){
+        boolean shell = true;
+        System.out.println("client Name?");
+        String clientName = scanner.nextLine();
+        System.out.println("date? (dd-mm-yy)");
+        String date = scanner.nextLine();
+        System.out.println("ordernummer?");
+        String orderNumber = scanner.nextLine();
+        quote quote = new quote(clientName, 0.0, date, orderNumber);
 
+        while(shell){
+
+        }
+
+
+        return quote;
+    }
+}
+
+class quote{
+    private String clientName;
+    private double price;
+    private String date;
+    private String orderNumber;
+    private boat boat;
+
+    quote(String clientName, double price, String date, String orderNumber){
+        clientName = this.clientName;
+        price = this.price;
+        date = this.date;
+        orderNumber = this.orderNumber;
+    }
+
+
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public boat getBoat() {
+        return boat;
+    }
+
+    public void setBoat(boat boat) {
+        this.boat = boat;
+    }
 }
 
 class boat{
-    motor motor;
-    frame frame;
-    hull hull;
+    private String type;
+    private String name;
+    private String serialNumber;
 
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 }
-
-// extraparts are for parts that are later added but we are not yet sure of what to do with them.
-class extraPart{
-
-}
-
-// optional parts are parts that can be chosen but are not required by the boats
-class optionalPart{
-
-}
-class sail extends optionalPart{
-
-}
-
-//essential parts are parts that everyboat must have
-class essentialPart{
-
-}
-
-
-class motor extends essentialPart {
-
-}
-
-
-
-class frame extends essentialPart{
-
-
-
-}
-
-class hull extends essentialPart{
-
-
-}
-
 
 /* this is a program that produces quotations complying to the conditions of the client "bedrijf 42"
 it should run in a while loop, and we intend to work with the basis of a template */
@@ -61,6 +107,7 @@ it should run in a while loop, and we intend to work with the basis of a templat
 
 public class Main {
     public static void main(String[] args) {
+        shell shell = new shell();
         boolean run = true;
         Scanner scanner = new Scanner(System.in);
         while(run) {
@@ -78,20 +125,10 @@ public class Main {
                     System.out.println("printing and storing quota before shutting down");
                     run = false;
                     break;
-                case "list":
-                    System.out.println("listing possible options with prices and discounts");
-                    break;
-                case "add":
-                    System.out.println("function for adding options");
-                    break;
-                case "remove":
-                    System.out.println("function for removing options");
-                    break;
-                case "option 1":
-                    System.out.println("selecting or de-selecting option 1");
-                    break;
-                case "option 2":
-                    System.out.println("selecting or de-selecting option 2");
+                case "create":
+                    System.out.println("creating quote");
+                    quote quote = shell.createQuote();
+
                     break;
                 default:
                     System.out.println("please choose an available option");
