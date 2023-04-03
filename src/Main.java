8b4624cb91aef80263357c1568f75d57a7eb7fba
BestGaner69 <ayhan_roj@live.nl>
2023-04-03 20:13:43 +0200

    lmao

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,16 @@ import java.text.DecimalFormat;
 class Option{
     private String name;
     private double price;
-    private boolean essential;
     private String description;
 
-    Option(String name, double price, boolean essential ,String description){
+    Option(String name, double price, String description){
         this.name = name;
         this.price = price;
-        this.essential = essential;
         this.description = description;
     }
-    Option(String name, double price,boolean essential){
+    Option(String name, double price){
         this.name = name;
         this.price = price;
-        this.essential = essential;
     }
 
     public void setDescription(String description) {
@@ -31,23 +28,19 @@ class Option{
     }
 }
 class OptionList {
-    Scanner scanner = new Scanner(System.in);
-
     private ArrayList<Option> essentialOptions;
     private ArrayList<Option> extraOptions;
 
-    Option Hull = new Option("hull", 2.0,true);
-    Option HullFrame = new Option("hull frame", 2.0,true);
-    Option deck = new Option("deck", 2.0,true);
-    Option cabin = new Option("cabin", 2.0,true);
-    Option LifeBuoys = new Option("life buoys", 2.0,true);
-    Option radio = new Option("radio", 2.0,true);
-    Option radars = new Option("radars", 2.0,true);
-    Option towerCranes = new Option("tower cranes", 2.0,true);
-    Option flagDecor = new Option("flags decoration", 2.0,true);
-
+    Option Hull = new Option("hull", 2.0);
+    Option HullFrame = new Option("hull frame", 2.0);
+    Option deck = new Option("deck", 2.0);
+    Option cabin = new Option("cabin", 2.0);
+    Option LifeBuoys = new Option("life buoys", 2.0);
+    Option radio = new Option("radio", 2.0);
+    Option radars = new Option("radars", 2.0);
+    Option towerCranes = new Option("tower cranes", 2.0);
+    Option flagDecor = new Option("flags decoration", 2.0);
     public OptionList() {
-
         essentialOptions = new ArrayList<Option>();
         essentialOptions.add(Hull);
         essentialOptions.add(HullFrame);
@@ -61,22 +54,12 @@ class OptionList {
         extraOptions.add(towerCranes);
         extraOptions.add(flagDecor);
     }
-
-    public Option createOption() {
-        System.out.println("Optienaam?");
-        String name = scanner.nextLine().strip();
-        System.out.println("Prijs?");
-        double price = scanner.nextDouble();
-        System.out.println("Is deze optie essentieel?");
-        boolean essential = scanner.hasNextBoolean();
-
-        Option option = new Option(name, price, essential);
-        if (essential) {
+    public void addOption(Option option, boolean isEssential) {
+        if (isEssential) {
             essentialOptions.add(option);
         } else {
             extraOptions.add(option);
         }
-        return option;
     }
 
 
@@ -105,33 +88,95 @@ class shell{
     public quote createQuote(){
         boolean shell = true;
         System.out.println("client Name?");
-        String clientName = scanner.nextLine().strip();
-        System.out.println("customer type");
-        String customerType = scanner.nextLine().strip();
+        String clientName = scanner.nextLine();
         System.out.println("date? (dd-mm-yy)");
         String date = scanner.nextLine();
         System.out.println("ordernummer?");
         String orderNumber = scanner.nextLine();
-        quote quote = new quote(clientName, customerType ,date, orderNumber);
+        quote quote = new quote(clientName, date, orderNumber);
         quote.setQuoteDetails();
-        String input = scanner.nextLine().strip();
-        switch (input) {
-            case "create" -> {
-                Option optiontest = this.optionList.createOption();
-            }
-            case " " -> {
 
-            }
-        }
         System.out.println("Voeg nieuwe opties toe? (ja/nee)");
         String answer = scanner.nextLine();
         while (answer.equalsIgnoreCase("ja")) {
-            Option option = this.optionList.createOption();
-            option.setDescription("Beschrijving van " + option.getName());
+            System.out.println("Optienaam?");
+            String name = scanner.nextLine();
+            System.out.println("Prijs?");
+            double price = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("Is deze optie essentieel? (ja/nee)");
+            boolean isEssential = scanner.nextLine().equalsIgnoreCase("ja");
+            Option option = new Option(name, price);
+            option.setDescription("Beschrijving van " + name);
+            optionList.addOption(option, isEssential);
             System.out.println("Voeg nog een optie toe? (ja/nee)");
             answer = scanner.nextLine();
         }
+
+        System.out.println("Voeg nieuwe klanttype toe? (ja/nee)");
+        answer = scanner.nextLine();
+        while (answer.equalsIgnoreCase("ja")) {
+            System.out.println("Klanttype?");
+            String customerType = scanner.nextLine();
+            boat.setCustomerType(customerType);
+            System.out.println("Voeg nog een klanttype toe? (ja/nee)");
+            answer = scanner.nextLine();
+        }
+
         return quote;
+    }
+}
+class Klant{
+    private String naam;
+    private Klantentype klantentype;
+
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public Klantentype getKlantentype() {
+        return klantentype;
+    }
+}
+abstract class Klantentype{
+    private String naam;
+    private double hoeveelheidkorting;
+
+
+    public Klantentype(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam(){
+        return naam;
+    }
+    public double getKorting(){
+        return hoeveelheidkorting;
+    }
+
+}
+class Particulier extends Klantentype{
+
+
+    public Particulier(String naam) {
+        super(naam);
+    }
+}
+
+class Bedrijf extends Klantentype{
+
+
+    public Bedrijf(String naam) {
+        super(naam);
+    }
+}
+
+class Overheid extends Klantentype{
+
+
+    public Overheid(String naam) {
+        super(naam);
     }
 }
 
@@ -139,7 +184,6 @@ class shell{
 class quote{
     Scanner scanner = new Scanner(System.in);
     private String clientName;
-    private String customerType;
     private String date;
     private String orderNumber;
     private boat boat;
@@ -149,10 +193,8 @@ class quote{
     private double totaalprijs;
     private double milieuKorting;
 
-
-    quote(String clientName, String customerType ,String date, String orderNumber){
+    quote(String clientName,  String date, String orderNumber){
         this.clientName = clientName;
-        this.customerType = customerType;
         this.date = date;
         this.orderNumber = orderNumber;
     }
@@ -256,14 +298,17 @@ it should run in a while loop, and we intend to work with the basis of a templat
 public class Main {
     private  OptionList optionlist = new OptionList();
     public static void main(String[] args) {
+        shell shell = new shell();
         boolean run = true;
         Scanner scanner = new Scanner(System.in);
         Main main = new Main();
         OptionList optionlist = new OptionList();
         optionlist.displayOptions();
+        shell.createQuote();
 
         while (run) {
             String input = scanner.nextLine(); //first version of inputting into console
+
             //switch case for a bar-bones version of commands and results, classes and methods have yet to be added.
             switch (input) {
                 case "exit":
@@ -277,7 +322,6 @@ public class Main {
                     run = false;
                     break;
                 case "create":
-                    shell shell = new shell();
                     System.out.println("creating quote");
                     quote quote = shell.createQuote();
                     break;
